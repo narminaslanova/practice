4923565e78247d1aeef9af9769986b58c2c239a5
narminaslanova <narminra@code.edu.az>
2022-01-17 12:24:42 +0200

    fragments and portal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 
-import AddUser from "./Components/Users/AddUser"
-import UsersList from "./Components/Users/UsersList"
-
+import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
 function App() {
   const [usersList, setUsersList] = useState([]);
 
@@ -16,10 +15,10 @@ function App() {
   };
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
--- src/Components/Users/UsersList.js
+++ src/Components/Users/UsersList.js
@@ -6,13 +6,17 @@ import classes from "./UsersList.module.css";
 const UsersList = (props) => {
   return (
     <Card className={classes.users}>
-      <ul>
-        {props.users.map((user) => (
-          <li key={user.id}>
-            {user.name} ({user.age} years old)
-          </li>
-        ))}
-      </ul>
+      {props.users.length > 0 ? (
+        <ul>
+          {props.users.map((user) => (
+            <li key={user.id}>
+              {user.name} ({user.age} years old)
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className={classes.users}>No users found</p>
+      )}
     </Card>
   );
 };
